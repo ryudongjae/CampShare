b4092201b6a5de822b2f68100a4e487533322c48
ryudongjae <rdj10149@gmail.com>
2021-07-29 16:18:38 +0900

    프로젝트 생성

---
+++ src/main/java/project/campshare/CampshareApplication.java
@@ -0,0 +1,13 @@
+package project.campshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampshareApplication.class, args);
+	}
+
+}
