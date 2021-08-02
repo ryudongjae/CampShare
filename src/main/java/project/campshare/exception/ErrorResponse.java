b723c9a36bf1ecc22e0e93c14efceb13cdfa785a
ryudongjae <rdj10149@gmail.com>
2021-08-02 16:48:44 +0900

    에외처리 (+수정)

---
+++ src/main/java/project/campshare/exception/ErrorResponse.java
@@ -0,0 +1,18 @@
+package project.campshare.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+@Getter
+public class ErrorResponse {
+
+    private HttpStatus status;
+    private String message;
+
+    @Builder
+    public ErrorResponse(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
--- src/main/java/project/campshare/exception/GlobalExceptionHandler.java
+++ src/main/java/project/campshare/exception/GlobalExceptionHandler.java
@@ -1,28 +1,55 @@
 package project.campshare.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(DuplicateEmailException.class)
-    protected ResponseEntity<String> duplicateEmailException(DuplicateEmailException e) {
-        return new ResponseEntity<String>("중복된 이메일입니다.", HttpStatus.BAD_REQUEST);
+    protected ResponseEntity<ErrorResponse> duplicateEmailException(DuplicateEmailException exception) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message("중복된 이메일 입니다.")
+                .build();
+
+        log.error(errorResponse.getMessage(),exception);
+        return new ResponseEntity<>(errorResponse,errorResponse.getStatus());
     }
 
     @ExceptionHandler(DuplicateNicknameException.class)
-    protected ResponseEntity<String> duplicateNicknameException(DuplicateNicknameException e) {
-        return new ResponseEntity<String>("중복된 닉네임입니다.", HttpStatus.BAD_REQUEST);
+    protected ResponseEntity<ErrorResponse> duplicateNicknameException(DuplicateNicknameException exception) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message("중복된 이메일 입니다.")
+                .build();
+
+        log.error(errorResponse.getMessage(),exception);
+
+        return new ResponseEntity<>(errorResponse,errorResponse.getStatus());
     }
 
     @ExceptionHandler(WrongConfirmPasswordException.class)
-    protected ResponseEntity<String> wrongConfirmPasswordException(WrongConfirmPasswordException e) {
-        return new ResponseEntity<String>("비밀번호 확인이 맞지 않습니다.", HttpStatus.BAD_REQUEST);
+    protected ResponseEntity<ErrorResponse> wrongConfirmPasswordException(WrongConfirmPasswordException exception){
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message("중복된 비밀번호 입니다.")
+                .build();
+        log.error(errorResponse.getMessage(),exception);
+        return new ResponseEntity<>(errorResponse,errorResponse.getStatus());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<String> methodArgumentNotValidException(MethodArgumentNotValidException e) {
-        return new ResponseEntity<String>(e.getFieldError().getDefaultMessage(), HttpStatus.BAD_REQUEST);
+    protected ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message(exception.getFieldError().getDefaultMessage())
+                .build();
+        log.error(errorResponse.getMessage(),exception);
+        return new ResponseEntity<>(errorResponse,errorResponse.getStatus());
     }
 }
