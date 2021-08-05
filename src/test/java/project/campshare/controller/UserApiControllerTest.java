dfdda70b535614bc376620b87736f6ce73b287a7
ryudongjae <rdj10149@gmail.com>
2021-08-05 15:41:16 +0900

    회원가입 테스트코드 추가

--- src/test/java/project/campshare/controller/UserApiControllerTest.java
+++ src/test/java/project/campshare/controller/UserApiControllerTest.java
@@ -9,13 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import project.campshare.Model.usermodel.user.User;
 import project.campshare.Model.usermodel.user.UserDto;
-import project.campshare.Model.usermodel.userrepository.UserRepository;
-import project.campshare.Model.usermodel.userservice.UserServiceImpl;
+import project.campshare.domain.repository.UserRepository;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -26,19 +22,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class UserApiControllerTest {
     @Autowired
     MockMvc mockMvc;
+    //실제 객체와 비슷하지만 테스트에 필요한 기능만 가지는 가짜 객체를 만들어서 애플리케이션
+    // 서버에 배포하지 않고도 스프링 MVC 동작을 재현할 수 있는 클래스를 의미합니다.
 
     @Autowired
     ObjectMapper objectMapper;
 
     @Autowired
-    UserRepository userRepository;
+     UserRepository userRepository;
 
     @Test
-    @DisplayName("회원 가입 성공")
+    @DisplayName("회원가입 성공")
     public void signUpSuccess() throws Exception {
         UserDto userDto = userDto = UserDto.builder()
                 .email("test123@test.com")
                 .password("test1234")
+                .confirmPassword("test1234")
                 .phone("01011112222")
                 .nickname("17171771")
                 .build();
@@ -49,12 +48,52 @@ class UserApiControllerTest {
                 .andDo(print())
                 .andExpect(status().isCreated())
                 .andExpect(header().string(HttpHeaders.LOCATION, "http://localhost/users/1"));
-        User resultUser = userRepository.findAll().get(0);
-        assertAll(
-                () -> assertThat(resultUser.getEmail()).isEqualTo("test123@test.com"),
-                () -> assertThat(resultUser.getNickname()).isEqualTo("17171771"),
-                () -> assertThat(resultUser.getPhone()).isEqualTo("01011112222")
-        );
     }
+    
+    
+    @Test
+    void 회원가입_실패()throws Exception{
+        //given
+        UserDto userDto =UserDto.builder()
+                .email("test123@naver.com")
+                .nickname("1111")
+                .password("test12")
+                .phone("01001012323")
+                .confirmPassword("test12")
+                .build();
+        //when
+        
+        //then
+        mockMvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userDto)))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    
+    }
+    
+    
+    @Test
+    @DisplayName("패스워드 불일치")
+    void passwordMissMatch()throws Exception{
+        //given
+        UserDto userDto  = UserDto.builder()
+                .email("rdj1014@naver.com")
+                .password("tt12")
+                .confirmPassword("tt123")
+                .phone("01012223334")
+                .nickname("1212")
+                .build();
+        //when
+        
+        //then
+        mockMvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userDto)))//ObjectMapper 클래스의 writeValueAsString,
+                // writeValueAsBytes는 Java 오브젝트로 부터 JSON을 만들고 이를 문자열 혹은 Byte 배열로 반환한다.
+                .andDo(print())
+                .andExpect(status().isBadRequest());
 
+    
+    }
 }
