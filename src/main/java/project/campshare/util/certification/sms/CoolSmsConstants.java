56b68692f4d2ba6c4bd25992ddc4797efbb68656
ryudongjae <rdj10149@gmail.com>
2021-08-18 03:22:11 +0900

    Api 변경

--- src/main/java/project/campshare/util/certification/sms/CoolSmsConstants.java
+++ src/main/java/project/campshare/util/certification/sms/CoolSmsConstants.java
@@ -4,9 +4,9 @@ package project.campshare.util.certification.sms;
  * coolSms
  */
 public class CoolSmsConstants {
-    public static String COOLSMS_KEY = "NCS6ZIOLCULQXVIT";
-    public static String COOLSMS_SECRET = "GDWQZCFZYLXYPIMEEFVPPZIFCUPEQN5T";
-    public static String OFFICIAL_PHONE = "01079422492";
+    public static String COOLSMS_KEY  ;
+    public static String COOLSMS_SECRET  ;
+    public static String OFFICIAL_PHONE  ;
     public static final String SMS_TYPE = "text";
     public static final String APP_VERSION = "test app 1.2";
 }
