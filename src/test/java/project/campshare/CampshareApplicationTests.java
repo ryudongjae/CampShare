505deaf4d24952413d35ad59ccd673f39fb452f2
ryudongjae <rdj10149@gmail.com>
2021-08-21 02:33:52 +0900

    Sourcetree Test

---
+++ src/test/java/project/campshare/CampshareApplicationTests.java
@@ -0,0 +1,15 @@
+package project.campshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootTest
+class CampshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
