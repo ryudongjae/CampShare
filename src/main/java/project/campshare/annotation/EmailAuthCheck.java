657488d262e32d341744fffc437ef61f305e677b
ryudongjae <rdj10149@gmail.com>
2021-08-21 03:13:17 +0900

    1.상품 등록,조회,수정,삭제 구현 2.인증 관련 상수 클래스로 관리 3.토큰 일치 여부 확인 후 인증 처리

---
+++ src/main/java/project/campshare/annotation/EmailAuthCheck.java
@@ -0,0 +1,14 @@
+package project.campshare.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target(METHOD)
+public @interface EmailAuthCheck {
+}
